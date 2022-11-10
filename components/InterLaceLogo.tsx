bdbeb13da375260c76f66e1bd983456030baa619
D3Portillo <d3portillo@gmail.com>
2022-11-10 06:03:29 +0100

    feat: add InterLace logo assets and update component

---
+++ components/InterLaceLogo.tsx
@@ -0,0 +1,8 @@
+import asset_logo from "@/assets/logo.svg"
+import Image from "next/image"
+
+function InterlaceLogo() {
+  return <Image height={42} src={asset_logo} alt="InterLace" />
+}
+
+export default InterlaceLogo
