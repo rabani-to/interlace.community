7481ba62f1c4c64865c0f62edc9f39f0ada73d57
D3Portillo <d3portillo@gmail.com>
2022-11-11 03:15:59 +0100

    enh(Button.tsx): add sharable flavor for atom component

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,12 +6,18 @@ function Button({
   className,
   fontSize,
   borderRadius,
+  isFormItem,
   children,
   isLink,
+  flavor,
+  isFull,
   ...props
 }: PropsWithChildren<
   {
     isLink?: boolean
+    flavor?: "violet"
+    isFull?: boolean
+    isFormItem?: boolean
     target?: string
     href?: string
     /** Your tw text-[size] class */
@@ -21,12 +27,16 @@ function Button({
   } & ButtonHTMLAttributes<{}>
 >) {
   const Wrapper = (isLink ? Link : "button") as any
+
   return (
     <Wrapper
       {...props}
       className={classnames(
         className,
-        fontSize || "text-xl",
+        isFormItem && "justify-center text-base font-bold",
+        flavor === "violet" && "bg-violet-600 text-white",
+        isFull && "w-full",
+        isFormItem ? "" : fontSize || "text-xl",
         borderRadius || "rounded-lg",
         "group flex items-center space-x-1 px-4 py-3 transition-all",
         "hover:ring-2 hover:opacity-90 focus:ring-4 ring-white/5 active:scale-95"
