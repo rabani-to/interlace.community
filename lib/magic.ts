d93f410c8a20026a86d347a2547f74e81c4edf06
D3Portillo <d3portillo@gmail.com>
2022-11-11 05:01:00 +0100

    enh(magic.ts): split to make #getMagicConnector atomic

--- lib/magic.ts
+++ lib/magic.ts
@@ -2,32 +2,34 @@ import { MagicAuthConnector } from "@everipedia/wagmi-magic-connector"
 import { chain } from "wagmi"
 
 const PUB_KEY = "pk_live_0B842C66A3876C25"
-export const rainbowMagicConnector = ({ chains }: any) =>
-  ({
-    id: "magic",
-    name: "Magic Link",
-    iconUrl: "/magic.png",
-    iconBackground: "#fff",
-    createConnector: () => {
-      const connector = new MagicAuthConnector({
-        chains,
-        options: {
-          apiKey: PUB_KEY,
-          accentColor: "#1E1E1E",
-          customHeaderText: "Continue with email",
-          oauthOptions: {
-            providers: ["google"],
-          },
-          magicSdkConfiguration: {
-            network: {
-              rpcUrl: chain.mainnet.rpcUrls.default,
-              chainId: chain.mainnet.id,
-            },
-          },
+export const getMagicConnector = (chains?: any[]) => {
+  return new MagicAuthConnector({
+    chains,
+    options: {
+      apiKey: PUB_KEY,
+      accentColor: "#1E1E1E",
+      customHeaderText: "Continue with email",
+      oauthOptions: {
+        providers: ["google"],
+      },
+      magicSdkConfiguration: {
+        network: {
+          rpcUrl: chain.mainnet.rpcUrls.default,
+          chainId: chain.mainnet.id,
         },
-      })
-      return {
-        connector,
-      }
+      },
     },
-  } as any)
+  }) as any
+}
+
+export const rainbowMagicConnector = ({ chains }: { chains?: any[] }) => ({
+  id: "magic",
+  name: "Magic Link",
+  iconUrl: "/magic.png",
+  iconBackground: "#fff",
+  createConnector: () => {
+    return {
+      connector: getMagicConnector(chains),
+    }
+  },
+})
