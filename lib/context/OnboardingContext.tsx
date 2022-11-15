9dad71be63a6aeb31f1bfb8a085e0e84f6de621b
D3Portillo <d3portillo@gmail.com>
2022-11-15 04:03:17 +0100

    feat(lib/context): add OnboardingContext to make session persistent registration

---
+++ lib/context/OnboardingContext.tsx
@@ -0,0 +1,47 @@
+import { Details, Experience, Preferences } from "@/types/shared"
+import {
+  type PropsWithChildren,
+  useState,
+  createContext,
+  useContext,
+} from "react"
+
+type OnboardingContextType = {
+  details?: Details
+  experience?: Experience
+  preferences?: Preferences
+  setStepData<T extends "details" | "experience" | "preferences">(
+    stepName: T,
+    data: T extends "details"
+      ? Details
+      : T extends "experience"
+      ? Experience
+      : Preferences
+  ): void
+}
+
+const OnboardingContext = createContext({} as OnboardingContextType)
+
+export const useOnboardingContext = () => useContext(OnboardingContext)
+
+export const OnboardingProvider = ({ children }: PropsWithChildren) => {
+  const [state, setState] = useState({} as OnboardingContextType)
+  console.log({ state })
+  return (
+    <OnboardingContext.Provider
+      value={{
+        ...state,
+        setStepData(stepName, data) {
+          setState((state) => ({
+            ...state,
+            [stepName]: data,
+          }))
+        },
+      }}
+    >
+      {children}
+    </OnboardingContext.Provider>
+  )
+}
+
+export default OnboardingContext
