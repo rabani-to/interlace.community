a2e2f4e632d71b8c6573c4ce231916cfa158b0f6
D3Portillo <d3portillo@gmail.com>
2022-11-17 01:37:47 +0100

    FEAT: add /create.ts api endpoint for profile creation

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -1,13 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
+import type { Profile } from "@/types/shared"
+import { Redis } from "@upstash/redis"
+import PROFILE, { PROFILE_DETAILS } from "@/lib/models/profile"
 
-type Data = {
-  name: string
-}
+const OMIT = [
+  PROFILE_DETAILS.profileImage,
+  PROFILE_DETAILS.refCode,
+  PROFILE_DETAILS.telegram,
+  PROFILE_DETAILS.twitter,
+]
+const ALL_PROFILE_KEYS = Object.keys(PROFILE).filter(
+  (key) => !OMIT.includes(key)
+)
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json(req.body)
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const user = req.body as Profile
+  const exitWithError = (status: number, error: string) =>
+    res.status(status).send({ error })
+
+  if (req.method === "POST") {
+    const redis = Redis.fromEnv()
+    for (let key of ALL_PROFILE_KEYS) {
+      const value = (user as any)[key]
+      if (!value || value.length < 1) {
+        return exitWithError(400, `Property @${key} is missing.`)
+      }
+    }
+    redis.set(user.address, user)
+    res.status(200).send({ status: "success" })
+  } else {
+    exitWithError(404, "Item not found")
+  }
 }
