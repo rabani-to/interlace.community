bf54bdc84fe62b3fa49546cd3f2e718c7b6b0fe1
D3Portillo <d3portillo@gmail.com>
2022-11-21 15:26:41 +0100

    feat(ModalShareProfile/): add modal item to allow user to share it's profile using different social platforms or link

---
+++ components/ModalShareProfile/ModalShareProfile.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image"
+
+import { FiCopy } from "react-icons/fi"
+import { FaInstagram, FaLinkedin, FaTelegram, FaTwitter } from "react-icons/fa"
+import PrimitiveDialog, { type DialogProps } from "@/components/PrimitiveModal"
+import Input from "@/components/forms/Input"
+
+import asset_bg from "@/assets/share-image.png"
+import styles from "./ModalShareProfile.module.css"
+
+function ModalShareProfile(props: DialogProps) {
+  return (
+    <PrimitiveDialog
+      maxWidth="max-w-3xl"
+      className={styles.withSvgBackground}
+      {...props}
+    >
+      <section className="flex">
+        <h3 className="text-center pt-12">
+          Share your Web3 profile with your social community
+        </h3>
+        <Image src={asset_bg} alt="" />
+      </section>
+      <section className="flex -mt-2 text-3xl text-white/70 items-center justify-center gap-4">
+        <button className="hover:text-white">
+          <FaLinkedin />
+        </button>
+        <button className="hover:text-white">
+          <FaInstagram />
+        </button>
+        <button className="hover:text-white">
+          <FaTwitter />
+        </button>
+        <button className="hover:text-white">
+          <FaTelegram />
+        </button>
+      </section>
+      <section className="max-w-lg mx-auto mt-4 mb-8">
+        <Input
+          className="bg-white"
+          readOnly
+          placeholder="https://interlace.community/profile/02ewo"
+          labelTextColor="text-white"
+          endEnhancer={
+            <button className="text-zinc-600 group px-2 flex space-x-1 items-center font-bold">
+              <FiCopy className="text-xl group-hover:scale-105 duration-75" />
+              <span>Copy</span>
+            </button>
+          }
+          label="Or copy link"
+        />
+      </section>
+    </PrimitiveDialog>
+  )
+}
+
+export default ModalShareProfile
