8d0c20df0ee470516c9b833fc761d1ab82ea2040
D3Portillo <d3portillo@gmail.com>
2022-11-23 21:57:19 +0100

    feat(ProfileCard.tsx): user now can see cached value and render values when update from modal

--- components/user/ProfileCard.tsx
+++ components/user/ProfileCard.tsx
@@ -1,4 +1,5 @@
-import { Fragment } from "react"
+import type { CardProfile } from "@/types/shared"
+import { Fragment, useEffect, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -6,7 +7,7 @@ import { FaLink, FaTachometerAlt } from "react-icons/fa"
 import { MdEdit, MdPayments } from "react-icons/md"
 import { RiBillFill } from "react-icons/ri"
 
-import useOnOffMachine from "@/lib/hooks/useOnOffMachine"
+import useOnOffMachine, { OnOffMachine } from "@/lib/hooks/useOnOffMachine"
 
 import asset_bg from "@/assets/bg.jpg"
 import ReactSelect from "@/components/forms/ReactSelect"
@@ -14,72 +15,14 @@ import Input from "@/components/forms/Input"
 import SectionForm from "./SectionForm"
 import Row, { RowItemIcon } from "./Row"
 
+const INIT_STATE: CardProfile = {} as any
 function ProfileCard() {
   const modalMachine = useOnOffMachine()
-
-  function handleSubmit() {
-    modalMachine.turnOff()
-  }
+  const [state, setState] = useState(INIT_STATE)
 
   return (
     <Fragment>
-      <SectionForm
-        onSubmit={handleSubmit}
-        title="Profile Overview"
-        onClose={modalMachine.turnOff}
-        show={modalMachine.isOn}
-      >
-        <Input
-          label="Your name"
-          name="name"
-          placeholder="You can remain `anon` 🙃"
-        />
-        <Input
-          label="Headline"
-          name="name"
-          required
-          placeholder="An awesome unicorn 🦄"
-        />
-        <ReactSelect
-          options={[
-            "1 - 8 hrs/week",
-            "8 - 12 hrs/week",
-            "12 - 25 hrs/week",
-            "25 - 32 hrs/week",
-            "32+ hrs/week",
-          ]}
-          required
-          label="Working Time"
-          placeholder="Select a range"
-        />
-        <Input
-          name="portfolio"
-          required
-          label="Website or portfolio"
-          placeholder="portfolio.xyz"
-          startEnhancer="https://"
-        />
-        <div className="flex flex-col md:flex-row justify-between gap-4 pt-1">
-          <div className="md:max-w-[10rem]">
-            <Input
-              required
-              label="Hourly rate (in USD)"
-              type="number"
-              step=".1"
-              min="1"
-              placeholder="Enter number"
-            />
-          </div>
-          <div className="flex-grow">
-            <ReactSelect
-              options={["Crypto", "FIAT"]}
-              required
-              label="Payment type"
-              placeholder="Select payment type"
-            />
-          </div>
-        </div>
-      </SectionForm>
+      <ModalPane machine={modalMachine} onSubmit={setState} initState={state} />
       <div className="w-full bg-white max-w-[30rem] rounded-xl p-4 pb-8 font-normal">
         <section className="relative bg-black rounded-xl overflow-hidden">
           <Row
@@ -87,27 +30,31 @@ function ProfileCard() {
             className="absolute ml-2 mt-2 md:mt-4 md:ml-4 bg-white/20 py-2 pr-6 pl-2 rounded-full backdrop-blur-lg"
           >
             <div className="bg-violet-200 w-10 h-10 rounded-full" />
-            <span className="text-white text-lg md:text-xl">Developer</span>
+            <span className="text-white text-lg md:text-xl">{state.role}</span>
           </Row>
           <Image placeholder="blur" src={asset_bg} alt="" />
         </section>
-        <h3 className="font-bold lg:text-4xl mt-6">AnonUser</h3>
-        <section className="flex flex-col items-start gap-4">
-          <h4 className="lg:text-xl font-bold">Backend Content Consultant</h4>
+        <h3 className="font-bold lg:text-4xl mt-6">
+          {state.name || "AnonUser"}
+        </h3>
+        {state.headline && <h4 className="lg:text-xl">{state.headline}</h4>}
+        <section className="flex flex-col items-start gap-4 mt-2">
           <Row gap="gap-4" className="flex-wrap-reverse">
-            <RowItemIcon Icon={MdPayments}>$60 USD/hour</RowItemIcon>
-            <RowItemIcon Icon={FaTachometerAlt}>10-20 hrs/week</RowItemIcon>
-            <RowItemIcon Icon={RiBillFill}>Crypto</RowItemIcon>
+            <RowItemIcon Icon={MdPayments}>
+              ${state.hourlyRate} USD/hour
+            </RowItemIcon>
+            <RowItemIcon Icon={FaTachometerAlt}>
+              {state.workingTime}
+            </RowItemIcon>
+            <RowItemIcon Icon={RiBillFill}>{state.paymentOptions}</RowItemIcon>
           </Row>
           <Link
             className="flex group items-center gap-2"
             target="_blank"
-            href="https://github.com/gyan0890"
+            href={state.portfolio || "#"}
           >
             <FaLink className="text-[#2924FF] text-2xl" />
-            <span className="group-hover:underline">
-              https://github.com/gyan0890
-            </span>
+            <span className="group-hover:underline">{state.portfolio}</span>
           </Link>
         </section>
         <section className="flex w-full -mb-2 mt-8 pt-6 border-t justify-center">
@@ -123,6 +70,125 @@ function ProfileCard() {
         </section>
       </div>
     </Fragment>
+  )
+}
+
+function ModalPane({
+  machine,
+  onSubmit,
+  initState,
+}: {
+  machine: OnOffMachine
+  initState: CardProfile
+  onSubmit(state: CardProfile): void
+}) {
+  const [state, setState] = useState(initState)
+  const asyncSetState = (newState: Partial<CardProfile>) =>
+    setState((prevState) => ({ ...prevState, ...newState }))
+
+  function handleSubmit() {
+    machine.turnOff()
+    onSubmit(state)
+  }
+
+  useEffect(() => {
+    setState(initState)
+  }, [machine.isOff, initState])
+
+  return (
+    <SectionForm
+      onSubmit={handleSubmit}
+      title="Profile Overview"
+      onClose={machine.turnOff}
+      show={machine.isOn}
+    >
+      <Input
+        defaultValue={state.name}
+        onChange={(name) =>
+          asyncSetState({
+            name,
+          })
+        }
+        label="Your name"
+        placeholder="You can remain `anon` 🙃"
+      />
+      <Input
+        defaultValue={state.headline}
+        onChange={(headline) =>
+          asyncSetState({
+            headline,
+          })
+        }
+        label="Headline"
+        placeholder="An awesome unicorn 🦄"
+      />
+      <ReactSelect
+        defaultValue={state.workingTime}
+        onSelect={([{ value: workingTime }]) =>
+          asyncSetState({
+            workingTime,
+          })
+        }
+        name="workingTime"
+        options={[
+          "1 - 8 hrs/week",
+          "8 - 12 hrs/week",
+          "12 - 25 hrs/week",
+          "25 - 32 hrs/week",
+          "32+ hrs/week",
+        ]}
+        required
+        label="Working Time"
+        placeholder="Select a range"
+      />
+      <Input
+        defaultValue={state.portfolio}
+        onChange={(portfolio) =>
+          asyncSetState({
+            portfolio,
+          })
+        }
+        name="portfolio"
+        required
+        label="Website or portfolio"
+        placeholder="portfolio.xyz"
+        startEnhancer="https://"
+      />
+      <div className="flex flex-col md:flex-row justify-between gap-4 pt-1">
+        <div className="md:max-w-[10rem]">
+          <Input
+            defaultValue={state.hourlyRate}
+            onChange={(hourlyRate) =>
+              asyncSetState({
+                hourlyRate,
+              })
+            }
+            required
+            name="hourlyRate"
+            label="Hourly rate (in USD)"
+            type="number"
+            step=".1"
+            min="1"
+            placeholder="Enter number"
+          />
+        </div>
+        <div className="flex-grow">
+          <ReactSelect
+            defaultValue={state.paymentOptions}
+            onSelect={([{ value: paymentOptions }]) =>
+              asyncSetState({
+                paymentOptions,
+              })
+            }
+            name="paymentOptions"
+            options={["Crypto", "FIAT"]}
+            required
+            label="Payment type"
+            placeholder="Select payment type"
+          />
+        </div>
+      </div>
+    </SectionForm>
   )
 }
 
