3bb09c49bedeb5cf8908124323f4c25fc8d7e623
D3Portillo <d3portillo@gmail.com>
2022-11-24 19:27:58 +0100

    FEAT: add profile/[id] view. Now can visit profiles

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,14 +1,19 @@
+import { useRouter } from "next/router"
 import SectionExpertise from "@/components/user/SectionExpertise"
 import SectionHowCanIContribute from "@/components/user/SectionHowCanIContribute"
 import SectionAboutMe from "@/components/user/SectionAboutMe"
 import ProfileCard from "@/components/user/ProfileCard"
 
+import useRemoteProfileData from "@/lib/hooks/useRemoteProfileData"
 import SeoTags from "@/components/SeoTags"
 import TopNavigation from "@/components/TopNavigation"
 import { LayoutItem } from "@/components/layouts/GradientSection"
 import Footer from "@/components/Footer"
 
 export default function ProfilePage() {
+  const router = useRouter()
+  const shortIdOrAddress = router.query.id as string
+  const profileData = useRemoteProfileData(shortIdOrAddress)
   return (
     <main data-type="texturized" className="bg-darker">
       <SeoTags title="InterLace | Dashboard" />
@@ -17,18 +22,50 @@ export default function ProfilePage() {
           <TopNavigation isHeadless />
         </LayoutItem>
       </section>
-      <LayoutItem>
-        <section className="flex mt-8 lg:mt-20 flex-col lg:flex-row text-black gap-12">
-          <div className="flex-grow" />
-          <ProfileCard isPublicView />
-          <div className="flex lg:mt-8 flex-col gap-12 flex-grow text-white">
-            <SectionHowCanIContribute isPublicView />
-            <SectionExpertise isPublicView />
-            <SectionAboutMe isPublicView />
-          </div>
-        </section>
-      </LayoutItem>
+      {profileData.isOk ? (
+        <LayoutItem>
+          <section className="flex mt-8 lg:mt-20 flex-col lg:flex-row text-black gap-12">
+            <div className="flex-grow" />
+            <ProfileCard isPublicView />
+            <div className="flex lg:mt-8 flex-col gap-12 flex-grow text-white">
+              <SectionHowCanIContribute isPublicView />
+              <SectionExpertise isPublicView />
+              <SectionAboutMe isPublicView />
+            </div>
+          </section>
+        </LayoutItem>
+      ) : (
+        <LoadingState />
+      )}
       <Footer className="mt-24 lg:mt-32" />
     </main>
+  )
+}
+
+function LoadingState() {
+  return (
+    <section className="min-h-[calc(40vh+6rem)] flex flex-col gap-2 items-center justify-center">
+      <svg
+        className="animate-spin h-12 w-12 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        />
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        />
+      </svg>
+      <span className="opacity-30">Wait a sec...</span>
+    </section>
   )
 }
