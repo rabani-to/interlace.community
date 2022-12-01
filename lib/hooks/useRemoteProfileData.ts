63db7f0fc4fb79522cdd8ae4588dc84f6530bb0e
D3Portillo <d3portillo@gmail.com>
2022-12-01 22:13:33 +0100

    chores(useRemoteProfileData.ts): setProfile to {} when shortIdOrAddress is not defined. rename setState to setProfile

--- lib/hooks/useRemoteProfileData.ts
+++ lib/hooks/useRemoteProfileData.ts
@@ -1,5 +1,6 @@
 import type { ProfileWithShort } from "@/types/shared"
 import type { RedisResponse } from "@/lib/redis"
+
 import { useLayoutEffect, useState } from "react"
 import ff from "@/lib/services/ff"
 import { noOp } from "@/lib/helpers"
@@ -7,7 +8,9 @@ import { noOp } from "@/lib/helpers"
 function useRemoteProfileData(shortIdOrAddress: string) {
   const [isLoading, setIsLoading] = useState(true)
   const [count, setCount] = useState(0)
-  const [state, setState] = useState<RedisResponse<ProfileWithShort>>({} as any)
+  const [profile, setProfile] = useState<RedisResponse<ProfileWithShort>>(
+    {} as any
+  )
   const revalidate = () => setCount((n) => n + 1)
 
   useLayoutEffect(() => {
@@ -15,21 +18,21 @@ function useRemoteProfileData(shortIdOrAddress: string) {
       ff.get<RedisResponse<ProfileWithShort>>(["/profile", shortIdOrAddress])
         .then((response) => {
           if (response.isOk) {
-            setState(response)
+            setProfile(response)
           }
         })
         .catch(noOp)
         .finally(() => {
           setIsLoading(false)
         })
-    }
+    } else setProfile({} as any)
   }, [shortIdOrAddress, count])
 
   return {
-    ...state,
+    ...profile,
     revalidate,
     isLoading,
-  } as typeof state & {
+  } as typeof profile & {
     revalidate(): void
     isLoading: boolean
   }
