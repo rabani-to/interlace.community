9d8bae27c2d7f335bc751afe3ac0ae319d11c78d
D3Portillo <d3portillo@gmail.com>
2022-12-01 22:14:22 +0100

    FEAT: user now has to click on cotinue action to create profile or view dashboard if already registered

--- pages/onboarding/index.tsx
+++ pages/onboarding/index.tsx
@@ -1,15 +1,17 @@
-import { Fragment, useEffect } from "react"
+import { Fragment } from "react"
 import Image from "next/image"
 import Link from "next/link"
-import { useConnectModal } from "@rainbow-me/rainbowkit"
+import { useAccountModal, useConnectModal } from "@rainbow-me/rainbowkit"
 import { useAccount, useConnect, useProvider } from "wagmi"
 import { useRouter } from "next/router"
 
 import { getMagicConnector } from "@/lib/magic"
-import { noOp } from "@/lib/helpers"
+import { beautifyAddress, noOp } from "@/lib/helpers"
+import useRemoteProfileData from "@/lib/hooks/useRemoteProfileData"
 import SeoTags from "@/components/SeoTags"
 import Button from "@/components/Button"
 
+import { BsArrowRight } from "react-icons/bs"
 import asset_stars from "@/assets/stars.svg"
 import asset_profilerow from "@/assets/profilerow-lg.png"
 
@@ -19,29 +21,37 @@ export default function Onboarding() {
   const { connect: connectWithMagicLink } = useConnect({
     connector: getMagicConnector(chains),
   })
-  const { isConnected, address } = useAccount()
+  const { address } = useAccount()
+  const remoteProfile = useRemoteProfileData(address!)
   const { openConnectModal = noOp } = useConnectModal()
+  const { openAccountModal = noOp } = useAccountModal()
+
   function handleConnectWallet() {
-    openConnectModal()
+    if (address) {
+      openAccountModal()
+    } else openConnectModal()
   }
 
   function handleConnectEmail() {
     connectWithMagicLink()
   }
-  const redirect = () => router.push("/onboarding/experience")
 
-  useEffect(() => {
-    if (isConnected && address) {
-      redirect()
+  function handleContinue() {
+    if (remoteProfile.isOk) {
+      // If remote profile exists redirect to dashboard view
+      router.push("/dashboard")
+    } else {
+      // Else we onboard the new user
+      router.push("/onboarding/experience")
     }
-  }, [isConnected])
+  }
 
   return (
     <Fragment>
       <SeoTags title="InterLace | Create your profile" />
       <main className="flex items-center h-screen text-zinc-800 font-normal">
-        <section className="w-full px-6 lg:w-1/2 h-full bg-white flex items-center justify-center">
-          <div className="w-full flex flex-col space-y-4 max-w-xs text-center">
+        <section className="w-full lg:overflow-auto px-6 lg:w-1/2 h-full bg-white flex items-center justify-center">
+          <div className="w-full pt-8 pb-4 flex flex-col space-y-4 max-w-xs text-center">
             <div>
               <h3 className="font-bold">Connect Wallet</h3>
               <p className="text-zinc-500 mt-2">
@@ -51,17 +61,35 @@ export default function Onboarding() {
             </div>
             <div className="flex flex-col space-y-2 pt-4">
               <Button flavor="violet" isFormItem onClick={handleConnectWallet}>
-                Connect wallet
+                {address
+                  ? `Connected (${beautifyAddress(address)})`
+                  : "Connect wallet"}
               </Button>
-              <span className="text-zinc-500">Or</span>
-              <Button
-                isFormItem
-                onClick={handleConnectEmail}
-                className="bg-zinc-100 text-black/70"
-              >
-                Use email instead
-              </Button>
+              {address ? null : (
+                <Fragment>
+                  <span className="text-zinc-500">Or</span>
+                  <Button
+                    isFormItem
+                    onClick={handleConnectEmail}
+                    className="bg-zinc-100 text-black/70"
+                  >
+                    Use email instead
+                  </Button>
+                </Fragment>
+              )}
             </div>
+            {address && (
+              <section className="pt-4">
+                <Button
+                  onClick={handleContinue}
+                  className="w-full border border-zinc-100 !gap-2"
+                  isFormItem
+                >
+                  <span>Continue</span>
+                  <BsArrowRight className="text-lg group-hover:translate-x-px" />
+                </Button>
+              </section>
+            )}
             <p className="text-zinc-500 text-sm pt-6">
               <span>Don{"'"}t have a wallet yet?</span>
               <br />
@@ -88,7 +116,7 @@ export default function Onboarding() {
           </p>
           <div className="flex pt-12 mx-4 items-center space-x-4">
             <Image height={42} src={asset_profilerow} alt="" />
-            <p>Join 2000+ Contributors</p>
+            <p>Join 1000+ Contributors</p>
           </div>
         </section>
       </main>
