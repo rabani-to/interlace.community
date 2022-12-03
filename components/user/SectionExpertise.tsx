b89cce960a416be8895b7a5c67249c91dd22a85e
D3Portillo <d3portillo@gmail.com>
2022-12-03 06:53:47 +0100

    feat(SectionExpertise.tsx): add signature request for profile update. update form inputs to match schema

--- components/user/SectionExpertise.tsx
+++ components/user/SectionExpertise.tsx
@@ -1,8 +1,10 @@
-import type { PublicProfileSection } from "@/types/shared"
-import { Fragment, useEffect, useState } from "react"
+import type { Experience, PublicProfileSection } from "@/types/shared"
+import type { DataWithSignature } from "./SectionFormPanel"
+import { Fragment, type PropsWithChildren } from "react"
 import { FaDiceD6 } from "react-icons/fa"
 
 import ALL_ROLES from "@/lib/models/roles"
+import { PROFILE_EXPERIENCE } from "@/lib/models/profile"
 
 import useOnOffMachine from "@/lib/hooks/useOnOffMachine"
 import ReactSelect from "@/components/forms/ReactSelect"
@@ -10,45 +12,54 @@ import ItemWithDescrition from "@/components/forms/ItemWithDescription"
 import SectionContainer from "./SectionContainer"
 import ButtonActionEmpty from "./ButtonActionEmpty"
 import SectionForm from "./SectionForm"
+import useExquesiteState from "./hook/useExquesiteState"
 
+type InitState = Pick<Experience, "role" | "expertise">
+const INIT_STATE: InitState = {
+  role: "",
+  expertise: [],
+}
 const ROLE_OPTIONS = Object.keys(ALL_ROLES)
 function SectionExpertise({ isPublicView, profile }: PublicProfileSection) {
   const modalMachine = useOnOffMachine()
-  const [role, setRole] = useState<string>()
-  const [areas, setAreas] = useState<string[]>([])
-  const [formAreas, setFormAreas] = useState<string[]>()
+  const [state, setState] = useExquesiteState(INIT_STATE, {
+    resetOnDeps: [profile?.address],
+    onMutateFormatter({ expertise, role }) {
+      return {
+        role: profile?.role || role,
+        expertise: profile?.expertise || expertise,
+      }
+    },
+  })
+  const { role, expertise } = state
   const EXPERTISE_OPTIONS = role ? ALL_ROLES[role].categories : []
+  const showEmptyState = expertise.length === 0
 
-  useEffect(() => {
-    setRole(profile?.role!)
-    setAreas(profile?.expertise || [])
-  }, [profile?.address])
-
-  function handleSubmit() {
-    setAreas((currentAreas) => formAreas || currentAreas)
+  function handleOnSubmit(signedProfile: DataWithSignature<InitState>) {
+    // TODO: add service to update profile
+    console.debug(
+      JSON.stringify({
+        raw: JSON.stringify(signedProfile.data),
+      })
+    )
     modalMachine.turnOff()
+    setState(signedProfile.data)
   }
 
-  function handleSelectAreas(areas: any[]) {
-    setFormAreas(areas.map(({ value }) => value))
-  }
-
-  const showEmptyState = areas.length === 0
   if (showEmptyState && isPublicView) return null
   return (
     <Fragment>
       <SectionForm
-        onSubmit={handleSubmit}
+        onSubmit={handleOnSubmit}
         title="Areas of expertise"
         onClose={modalMachine.turnOff}
         show={modalMachine.isOn}
       >
         <ReactSelect
+          required
           options={ROLE_OPTIONS}
           defaultValue={role}
-          onSelect={([{ value }]) => setRole(value)}
-          name="role"
-          required
+          name={PROFILE_EXPERIENCE.role}
           label="What role are you looking for?"
           placeholder="Select role"
         />
@@ -56,11 +67,10 @@ function SectionExpertise({ isPublicView, profile }: PublicProfileSection) {
           description={role ? "Pick up to 4." : "You must select a role."}
         >
           <ReactSelect
-            options={EXPERTISE_OPTIONS}
-            defaultValue={areas.length ? areas : undefined}
-            onSelect={handleSelectAreas}
-            name="areas"
             required
+            options={EXPERTISE_OPTIONS}
+            defaultValue={expertise.length ? expertise : undefined}
+            name={PROFILE_EXPERIENCE.expertise}
             label="Areas of expertise"
             placeholder="Select one or more"
             isMulti
@@ -79,19 +89,23 @@ function SectionExpertise({ isPublicView, profile }: PublicProfileSection) {
               Add expertise
             </ButtonActionEmpty>
           )}
-          {areas.map((areaName) => {
-            return (
-              <li
-                key={`user-area-${areaName}`}
-                className="px-3 py-1 border-2 border-white rounded-full text-xl font-normal"
-              >
-                {areaName}
-              </li>
-            )
+          {expertise.map((name) => {
+            return <Expertise key={`expertise-${name}`}>{name}</Expertise>
           })}
         </ul>
       </SectionContainer>
     </Fragment>
+  )
+}
+
+function Expertise({ children }: PropsWithChildren) {
+  return (
+    <li
+      key={`user-area-${children}`}
+      className="px-3 py-1 border-2 border-white rounded-full text-xl font-normal"
+    >
+      {children}
+    </li>
   )
 }
 
