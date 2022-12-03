d47bee2e547039bfe5120d9d491636c536f2d21f
D3Portillo <d3portillo@gmail.com>
2022-12-03 21:47:03 +0100

    chores(models/): add currency,time models

---
+++ lib/models/currency.ts
@@ -0,0 +1,1 @@
+export const PAYMENT_TYPES = ["Crypto", "FIAT"]
---
+++ lib/models/time.ts
@@ -0,0 +1,9 @@
+export const HOURLY_RATES = [
+  "1 - 8 hrs/week",
+  "8 - 12 hrs/week",
+  "12 - 25 hrs/week",
+  "25 - 32 hrs/week",
+  "32+ hrs/week",
+]
+
+export const COMMITMENT_TIME = ["Salaried", "Freelance", "Open Time Commitment"]
