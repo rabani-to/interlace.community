1f574c7941132dd4bfc1b4b7f2948dbb309ad1eb
D3Portillo <d3portillo@gmail.com>
2022-12-19 00:53:34 +0100

    chores: style magic.link

--- @types/globals.d.ts
+++ @types/globals.d.ts
@@ -1,5 +1,9 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     NEXT_PUBLIC_FILESTACK_API: string
+    /** When `true` will output sourcemaps
+     * @see https://nextjs.org/docs/advanced-features/source-maps
+     */
+    SOURCE_MAPS?: "true"
   }
 }
