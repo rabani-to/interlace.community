f2c201927e6fc574d880ef64e64ba729e9f1398e
D3Portillo <d3portillo@gmail.com>
2023-05-14 18:34:31 +0200

    fix(useHybridAccount): force user to re-connect, disconnect now simply reloads page

--- lib/hooks/useHybridAccount.ts
+++ lib/hooks/useHybridAccount.ts
@@ -1,56 +1,17 @@
-import { useEffect, useState } from "react"
 import { useConnectModal } from "@rainbow-me/rainbowkit"
-import { useAccount, useDisconnect } from "wagmi"
-import { getNearWalletConnection } from "@/lib/near"
+import { useAccount } from "wagmi"
 import { noOp } from "@/lib/helpers"
-
-const INIT_STATE = {
-  address: "",
-  disconnect: noOp,
-}
 
 function useHybridAccount() {
   const { openConnectModal = noOp } = useConnectModal()
-  const [account, setAccount] = useState(INIT_STATE)
-  const wagmiAccount = useAccount()
-
-  const { disconnect: wagmiDisconnect } = useDisconnect()
-
-  const makeDisconnect = (disconnect: () => void) => {
-    return function proxyDisconnect() {
-      setAccount(() => {
-        disconnect()
-        return INIT_STATE
-      })
-    }
-  }
-
-  useEffect(() => {
-    if (wagmiAccount.isConnected && wagmiAccount.address) {
-      setAccount({
-        address: wagmiAccount.address,
-        disconnect: makeDisconnect(wagmiDisconnect),
-      })
-    } else {
-      // handle metamask wallet change - reset state
-      setAccount(INIT_STATE)
-    }
-  }, [wagmiAccount.address])
-
-  useEffect(() => {
-    getNearWalletConnection().then((wallet) => {
-      if (wallet.isSignedIn()) {
-        setAccount({
-          address: wallet.getAccountId(),
-          disconnect: makeDisconnect(wallet.signOut),
-        })
-      }
-    })
-  }, [])
+  const { address } = useAccount()
 
   return {
-    ...account,
+    address,
     openConnectModal,
+    disconnect: () => {
+      location.reload()
+    },
   }
 }
 
